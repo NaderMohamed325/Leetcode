a197515db785dd650325f07b278a21a06f5d0701
Nader Mohamed Nader <nadoorclasher@gmail.com>
2024-11-27 17:46:15 +0200

    [704] [Time Beats: 100.00%] [Memory Beats: 26.03%] - LeetPush

--- 704-Binary-Search.cpp
+++ 704-Binary-Search.cpp
@@ -1,13 +1,9 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 class Solution {
 public:
     int search(vector<int> &nums, int target) {
-        int l = 0, r = nums.size() - 1;
+        long long l = 0, r = nums.size() - 1;
         while (l <= r) {
-            int mid = l + (r - l) / 2;
+            long long mid = l + (r - l) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
